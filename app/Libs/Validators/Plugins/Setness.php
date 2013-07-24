9218e9672c8d7bfe3522ce1267eaddbedc922547
ivorobioff <ivorobioff@mail.roboforex.dev>
2013-07-24 12:10:47 +0300

    changes

---
+++ app/Libs/Validators/Plugins/Setness.php
@@ -0,0 +1,45 @@
+<?php
+class Libs_Validators_Plugins_Setness extends Libs_Validators_Plugins
+{
+	private $_required_fields;
+
+	private $_fields;
+
+	public function setRequiredFields(array $fields)
+	{
+		$this->_required_fields = $fields;
+		return $this;
+	}
+
+	public function setFields(array $fields)
+	{
+		$this->_fields = $fields;
+		return $this;
+	}
+
+	public function check()
+	{
+		if (!$this->_required_fields) return true;
+
+		foreach ($this->_required_fields as $item)
+		{
+			if (!isset($this->_fields[$item])) return false;
+		}
+
+		return true;
+	}
+
+	public function getMissingFields()
+	{
+		$fields = array();
+
+		if (!$this->_required_fields) return array();
+
+		foreach ($this->_required_fields as $item)
+		{
+			if (!isset($this->_fields[$item])) $fields[] = $item;
+		}
+
+		return $fields;
+	}
+}
